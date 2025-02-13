0098c23614dde1390296c3893f86260778fa0284
ShahhNasir <shahhnasir83@gmail.com>
2025-02-14 01:29:49 +0530

    data ingestion done

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,5 @@
 import sys
-import logging
-
+from src.logger import logging
 def error_message_details(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
@@ -17,9 +16,4 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__ == "__main__":
-    try:
-        10/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e,sys)
+
